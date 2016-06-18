205ccb43a0a438ee3b57ff2c3f89f984346c7268
Rafi Taub <taubrafi@gmail.com>
2016-06-19 02:33:51 +0300

    Added DS2401+ files without adding code to run it.
    Added DS2401+ files without adding code to run it.

---
+++ CC3200_workspace/ProjectV3/DS2401.h
@@ -0,0 +1,14 @@
+/*
+ * OneWire.h
+ *
+ *  Created on: Sep 7, 2014
+ *      Author: Rafi
+ */
+
+#ifndef ONEWIRE_H_
+#define ONEWIRE_H_
+
+int ds2401_get_number(unsigned char id[6]);
+
+
+#endif /* ONEWIRE_H_ */
